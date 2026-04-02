5e403a226f18e184c4d28a258921a76c329a1367
Mateusz Kisielewski <254779@edu.p.lodz.pl>
2026-04-02 17:50:44 +0200

    a*

--- zadanie_1/algorithms.py
+++ zadanie_1/algorithms.py
@@ -1,13 +1,14 @@
+import heapq
 from collections import deque
 from manager import koncowy_efekt_ukladanki, pobierz_sasiadow
 
 def bfs(plansza_startowa, wiersze, kolumny, porzadek_sprawdzania):
     wzor = koncowy_efekt_ukladanki(wiersze, kolumny)
-    
+
     stany_odwiedzone = 1
     stany_przetworzone = 0
     max_glebokosc = 0
-    
+
     if plansza_startowa == wzor:
         return "", stany_odwiedzone, stany_przetworzone, max_glebokosc
 
@@ -16,9 +17,9 @@ def bfs(plansza_startowa, wiersze, kolumny, porzadek_sprawdzania):
 
     while kolejka:
         aktualna_plansza, sciezka, glebokosc = kolejka.popleft()
-        
+
         stany_przetworzone += 1
-        
+
         if glebokosc > max_glebokosc:
             max_glebokosc = glebokosc
 
@@ -28,17 +29,17 @@ def bfs(plansza_startowa, wiersze, kolumny, porzadek_sprawdzania):
             ostatni_ruch = ""
 
         sasiedzi = pobierz_sasiadow(aktualna_plansza, wiersze, kolumny, porzadek_sprawdzania, ostatni_ruch)
-        
+
         for nowa_plansza, ruch in sasiedzi:
             if nowa_plansza == wzor:
                 stany_odwiedzone += 1
                 return sciezka + ruch, stany_odwiedzone, stany_przetworzone, max_glebokosc
-            
+
             if nowa_plansza not in odwiedzone:
                 odwiedzone.add(nowa_plansza)
                 stany_odwiedzone += 1
                 kolejka.append((nowa_plansza, sciezka + ruch, glebokosc + 1))
-                
+
     return None, stany_odwiedzone, stany_przetworzone, max_glebokosc
 
 def dfs(plansza_startowa, wiersze, kolumny, porzadek_sprawdzania):
@@ -88,7 +89,7 @@ def dfs(plansza_startowa, wiersze, kolumny, porzadek_sprawdzania):
 
 
 def astar(plansza, wiersze, kolumny, parametr):
-    def manhattan(plansza, wiersze, kolumny, parametr):
+    def manhattan(plansza):
         odleglosc = 0
         for i, kafelek in enumerate(plansza):
             if kafelek == 0:
@@ -101,7 +102,7 @@ def astar(plansza, wiersze, kolumny, parametr):
             odleglosc += abs(wiersz_aktualny - wiersz_docelowy) + abs(kolumna_aktualna - kolumna_docelowa)
         return odleglosc
 
-    def hamming(plansza, wiersze, kolumny, parametr):
+    def hamming(plansza):
         odleglosc = 0
         for i, kafelek in enumerate(plansza):
             if kafelek == 0:
@@ -110,7 +111,48 @@ def astar(plansza, wiersze, kolumny, parametr):
                 odleglosc += 1
         return odleglosc
 
+    wzor = koncowy_efekt_ukladanki(wiersze, kolumny)
+    stany_odwiedzone = 1
+    stany_przetworzone = 0
+    max_glebokosc = 0
 
+    if parametr == "manhattan":
+        heurystyka = manhattan
+    elif parametr == "hamming":
+        heurystyka = hamming
 
+    if plansza == wzor:
+        return "", stany_odwiedzone, stany_przetworzone, max_glebokosc
 
+    h=heurystyka(plansza)
+    kolejka=[(h, 0, plansza, "")]
+    odwiedzone = {plansza}
 
+    while kolejka:
+        f, g, aktualna_plansza, sciezka = heapq.heappop(kolejka)
+
+        stany_przetworzone += 1
+
+        if g > max_glebokosc:
+            max_glebokosc = g
+
+        if len(sciezka) > 0:
+            ostatni_ruch = sciezka[-1]
+        else:
+            ostatni_ruch = ""
+
+        sasiedzi = pobierz_sasiadow(aktualna_plansza, wiersze, kolumny, "LRUD", ostatni_ruch)
+
+        for nowa_plansza, ruch in sasiedzi:
+            if nowa_plansza == wzor:
+                stany_odwiedzone += 1
+                return sciezka + ruch, stany_odwiedzone, stany_przetworzone, max_glebokosc
+
+            if nowa_plansza not in odwiedzone:
+                odwiedzone.add(nowa_plansza)
+                stany_odwiedzone += 1
+                nowe_g = g + 1
+                nowe_f = nowe_g + heurystyka(nowa_plansza)
+                heapq.heappush(kolejka, (nowe_f, nowe_g, nowa_plansza, sciezka + ruch))
+
+    return None, stany_odwiedzone, stany_przetworzone, max_glebokosc
--- zadanie_1/program.py
+++ zadanie_1/program.py
@@ -9,18 +9,18 @@ def main():
         print("Użycie: .\program <strategia> <parametr> <plik_wej> <plik_roz> <plik_stat>")
         sys.exit(1)
 
-strategia = sys.argv[1]
-parametr = sys.argv[2]
-plik_wejsciowy = sys.argv[3]
-plik_rozwiazanie = sys.argv[4]
-plik_statystyki = sys.argv[5]
+    strategia = sys.argv[1]
+    parametr = sys.argv[2]
+    plik_wejsciowy = sys.argv[3]
+    plik_rozwiazanie = sys.argv[4]
+    plik_statystyki = sys.argv[5]
 
-tekst = wczytaj_tekst_z_pliku(plik_wejsciowy)
-plansza, wiersze, kolumny = zbuduj_plansze_z_tekstu(tekst)
+    tekst = wczytaj_tekst_z_pliku(plik_wejsciowy)
+    plansza, wiersze, kolumny = zbuduj_plansze_z_tekstu(tekst)
 
-poczatek_czas = time.perf_counter()
+    poczatek_czas = time.perf_counter()
 
-match strategia:
+    match strategia:
         case 'bfs':
             wynik = bfs(plansza, wiersze, kolumny, parametr)
         case 'dfs':
@@ -32,10 +32,10 @@ match strategia:
             sys.exit(1)
 
 
-koniec_czas = time.perf_counter()
-czas_obliczen = (koniec_czas - poczatek_czas) * 1000
+    koniec_czas = time.perf_counter()
+    czas_obliczen = (koniec_czas - poczatek_czas) * 1000
 
-sciezka, stany_odwiedzone, stany_przetworzone, max_glebokosc = wynik
-zapisz_wyniki(plik_rozwiazanie, plik_statystyki, sciezka, stany_odwiedzone, stany_przetworzone, max_glebokosc, czas_obliczen)
+    sciezka, stany_odwiedzone, stany_przetworzone, max_glebokosc = wynik
+    zapisz_wyniki(plik_rozwiazanie, plik_statystyki, sciezka, stany_odwiedzone, stany_przetworzone, max_glebokosc, czas_obliczen)
 
 main()
