dbf0c8644991a6fd939548ff13590c122f857718
Krzysztof Kata <254776@edu.p.lodz.pl>
2026-05-18 21:27:08 +0200

    Add model configuration and training history files
    - Created a new JSON file `model_siec_irysy.json` containing model weights and biases with bias enabled.
    - Added a CSV file `temp_historia.csv` to log training history with epochs and corresponding main error values.

---
+++ zadanie_2/charts.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import io
+
+def rysuj_wykres_mse(plik_wejsciowy, plik_wyjsciowy):
+    with open(plik_wejsciowy, 'r') as f:
+        linie = f.readlines()
+        
+    czyste_dane = []
+    for linia in linie:
+        if linia.strip() == '' or linia.startswith('Log:'):
+            break
+        czyste_dane.append(linia.strip())
+        
+    dane_jako_tekst = '\n'.join(czyste_dane)
+    
+    df = pd.read_csv(io.StringIO(dane_jako_tekst), sep=r'\s*,\s*', engine='python')
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(df['Epoka'], df['blad_glowny'], marker='o', linestyle='-', color='b', markersize=4)
+    
+    plt.title('Spadek błędu globalnego (MSE) w procesie nauki')
+    plt.xlabel('Epoka')
+    plt.ylabel('Błąd globalny (MSE)')
+    plt.grid(True)
+    plt.tight_layout()
+    
+    plt.savefig(plik_wyjsciowy)
+    plt.show()
--- zadanie_2/main.py
+++ zadanie_2/main.py
@@ -1,12 +1,14 @@
 from zadanie1_irysy import siec_irysy
 from zadanie2_autoenkoder import siec_autoenkoder
+from charts import rysuj_wykres_mse
 
 def main():
     while(True):
         print("\nWybierz zadanie:")
         print("1 - Klasyfikacja irysów")
         print("2 - Autoenkoder")
-        print("3 - Zakończ")
+        print("3 - Rysuj wykres MSE z logu")
+        print("4 - Zakończ")
 
         wybor = input("\nWybór: ")
 
@@ -16,6 +18,10 @@ def main():
             case "2":
                 siec_autoenkoder()
             case "3":
+                nazwa_pliku_wejscia = input("Podaj nazwę pliku logu (np. 'log_irysy.txt'): ")
+                nazwa_pliku_wyjsciowego = input("Podaj nazwę pliku wyjściowego dla wykresu (np. 'wykres_mse.png'): ")
+                rysuj_wykres_mse(nazwa_pliku_wejscia, nazwa_pliku_wyjsciowego)
+            case "4":
                 print("Koniec programu")
                 break
             case _:
